7e09b5031a2efe1086c6d9d8d8af8204722ada2c
klavier <1255854195@qq.com>
2026-04-03 19:05:00 +0800

    add robot As2 and H2

--- src/assets/robots/__init__.py
+++ src/assets/robots/__init__.py
@@ -6,6 +6,10 @@ from .unitree_a2.a2_constants import (
   get_a2_robot_cfg as get_a2_robot_cfg,
 )
 
+from .unitree_as2.as2_constants import (
+  get_as2_robot_cfg as get_as2_robot_cfg,
+)
+
 from .unitree_g1.g1_constants import (
   G1_ACTION_SCALE as G1_ACTION_SCALE,
 )
@@ -32,4 +36,11 @@ from .unitree_h1_2.h1_2_constants import (
 )
 from .unitree_h1_2.h1_2_constants import (
   get_h1_2_robot_cfg as get_h1_2_robot_cfg,
+)
+
+from .unitree_h2.h2_constants import (
+  H2_ACTION_SCALE as H2_ACTION_SCALE,
+)
+from .unitree_h2.h2_constants import (
+  get_h2_robot_cfg as get_h2_robot_cfg,
 )
